0c04a89f649d93f49c6e3c2abb40133483209836
John Landrum <john.landrum.26@gmail.com>
2025-04-04 18:04:12 +0000

    First Implementations of known methods in DAO

--- src/main/java/DAO/DAO.java
+++ src/main/java/DAO/DAO.java
@@ -1,5 +1,8 @@
 package DAO;
+
 import Util.ConnectionUtil;
+import Model.Account;
+import Model.Message;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,5 +12,73 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DAO {
-    
+    public List<Message> getAllMessages() {
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM message";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Message message = new Message(rs.getInt("message_id"), rs.getInt("posted_by"),
+                rs.getString("message_text"), rs.getLong("time_posted_epoch"));
+                messages.add(message);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return messages;
+    }
+
+    public Account getAccount(String username) {
+        Connection connection = ConnectionUtil.getConnection();
+        ;
+    }
+
+    public Account createAccount(Account account) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "INSERT INTO account (account_id, username, password) VALUES (?, ?, ?);" ;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setInt(1,account.getAccount_id());
+            preparedStatement.setString(2,account.getUsername());
+            preparedStatement.setString(3,account.getPassword());
+            
+            preparedStatement.executeUpdate();
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if(pkeyResultSet.next()){
+                int generated_account_id = (int) pkeyResultSet.getLong(1);
+                return new Account(generated_account_id, account.getUsername(), account.getPassword());
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+    }
+
+    public Message createMessage(Message message) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "INSERT INTO message (message_id, posted_by, message_text, time_posted_epoch) VALUES (?, ?, ?, ?);" ;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setInt(1,message.getMessage_id());
+            preparedStatement.setInt(2,message.getPosted_by());
+            preparedStatement.setString(3,message.getMessage_text());
+            preparedStatement.setLong(4,message.getTime_posted_epoch());
+            
+            preparedStatement.executeUpdate();
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if(pkeyResultSet.next()){
+                int generated_message_id = (int) pkeyResultSet.getLong(1);
+                return new Message(generated_message_id, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+    }
 }
